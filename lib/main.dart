543a08984bb7e61e59a3581cc1bd6a67c2899c81
HanZawNyein <HanZawNyein>
2023-04-23 15:40:15 +0630

    add create acc and signin

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:amazon_clone_tutorial/constants/global_variables.dart';
+import 'package:amazon_clone_tutorial/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone_tutorial/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Clone',
+      theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme:
+              ColorScheme.light(primary: GlobalVariables.secondaryColor),
+          appBarTheme: AppBarTheme(
+              elevation: 0.0, iconTheme: IconThemeData(color: Colors.black))),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: AuthScreen(),
+    );
+  }
+}
