1c97bc81405accf7af5834bafbf8f9c138f197e2
HanZawNyein <HanZawNyein>
2023-04-24 08:57:12 +0630

    add addressbox

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -2,8 +2,12 @@ import 'package:amazon_clone_tutorial/provider/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../constants/global_variables.dart';
+import '../widgets/address_box.dart';
+
 class HomeScreen extends StatefulWidget {
   static const String routeName = "/home";
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
@@ -15,8 +19,76 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
-      body: Center(
-        child: Text(user.toJson(),),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: AppBar(
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: GlobalVariables.appBarGradient,
+              ),
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                      height: 42,
+                      margin: EdgeInsets.only(left: 15),
+                      alignment: Alignment.topLeft,
+                      child: Material(
+                        borderRadius: BorderRadius.circular(7),
+                        elevation: 1,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: InkWell(
+                              onTap: () {},
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 6),
+                                child: Icon(
+                                  Icons.search,
+                                  color: Colors.black,
+                                  size: 23,
+                                ),
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: EdgeInsets.only(top: 10),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(7)),
+                              borderSide: BorderSide.none,
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(7)),
+                                borderSide: BorderSide(
+                                    color: Colors.black38, width: 1)),
+                            hintText: "Search Amazon.in",
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 17,
+                            )
+                          ),
+                        ),
+                      )),
+                ),
+                Container(
+                  color: Colors.transparent,
+                  height: 42,
+                  margin: EdgeInsets.symmetric(horizontal: 10),
+                  child: Icon(Icons.mic,color: Colors.black,size: 25,),
+                ),
+              ],
+            ),
+          )),
+      body: Column(
+        children:[
+          AddressBox(),
+          // Text(
+          //   user.toJson(),
+          // ),
+        ]
       ),
     );
   }
